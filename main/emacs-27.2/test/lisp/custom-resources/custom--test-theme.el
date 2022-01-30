5f74e634f1a1ee56847335068368d49348a8daa1
Jtobyy <jtobi8161@gmail.com>
2022-01-30 08:29:01 -0500

    Initail commit

--- main/emacs-27.2/test/lisp/custom-resources/custom--test-theme.el
+++
@@ -1,9 +0,0 @@
-(deftheme custom--test
-  "A test theme.")
-
-(custom-theme-set-variables
- 'custom--test
- '(custom--test-user-option 'bar)
- '(custom--test-variable 'bar))
-
-(provide-theme 'custom--test)
