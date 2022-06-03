f798bbbdd2993974a91a88c767687230977c976d
Jtobyy <jtobi8161@gmail.com>
2022-06-04 00:33:00 +0100

    Modify admin for Partner Classes

--- main/admin.py
+++ main/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
+ 
+from models import Customer, Clothe, PendingReg, Fabric, Address, Partner
 
-from .models import Customer, Tailor, Clothe, Seller, PendingTailorReg, PendingSellerReg, Fabric
+def PendingRegAdmin():
+    '''PendingRegAdmin'''
+    list_display = ('business_name', 'brand_type')
+    change_form_template = 'admin/pendingreg/change_form.html'
+admin.site.register(PendingReg, PendingRegAdmin)
 
-# Register your models here.
+def PartnerAdmin():
+    '''PartnerAdmin'''
+    list_display = ('business_name', 'brand_type', 'legal_rep_last_name')
+admin.site.register(Partner, PartnerAdmin)
+
 admin.site.register(Customer)
-admin.site.register(Tailor)
+admin.site.register(Address)
 admin.site.register(Clothe)
-admin.site.register(Seller)
-admin.site.register(PendingTailorReg)
-admin.site.register(PendingSellerReg)
 admin.site.register(Fabric)
